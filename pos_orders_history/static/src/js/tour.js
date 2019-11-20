a25b1dbe9769b74589640c8ae4dc4aa870095b3a
KolushovAlexandr <kolushov@it-projects.info>
2019-11-20 20:03:55 +0500

    :arrow_up::one::three: pos_orders_history

--- pos_orders_history/static/src/js/tour.js
+++ pos_orders_history/static/src/js/tour.js
@@ -124,18 +124,15 @@ odoo.define('pos_orders_history.tour', function(require) {
                 content: "<p>Click to start the point of sale interface. It <b>runs on tablets</b>, laptops, or industrial hardware.</p><p>Once the session launched, the system continues to run without an internet connection.</p>",
                 position: "bottom"
             },{
-                trigger: '.o_main_content:has(.loader:hidden)',
-                content: 'waiting for loading to finish',
+                content: "Switch to table or make dummy action",
+                trigger: '.table:not(.oe_invisible .neworder-button), .order-button.selected',
                 timeout: 20000,
-                run: function () {
-                    // it's a check
-                },
             }];
         },
 
         select_table: function() {
             return {
-                content: "Switch to table or make dummy action",
+                content: "Switch to table or make dummy action"
                 trigger: '.table:not(.oe_invisible .neworder-button), .order-button.selected',
                 position: "bottom"
             };
